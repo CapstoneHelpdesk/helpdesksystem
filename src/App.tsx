d87d7b719252ffb5264db2f9276779a82c20d99b
CapstoneHelpdesk <helpdeskcapstonesystem@gmail.com>
2025-04-13 17:25:28 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,1 @@
-import { Sidebar, SidebarProvider } from "./components/ui/sidebar"; // Pastikan path benar
-import DashboardAdmin from "./components/DashboardAdmin";
 
-export default function App() {
-  return (
-    <SidebarProvider>
-      <div className="flex min-h-screen">
-        <Sidebar variant="floating" collapsible="icon">
-          <div className="p-4">📂 Files</div>
-          <div className="p-4">⚙️ Settings</div>
-          <div className="p-4">📊 Reports</div>
-        </Sidebar>
-        <DashboardAdmin />
-      </div>
-    </SidebarProvider>
-  );
-}
